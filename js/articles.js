aa87c3648da12e595cbe474f16d86cd0272d91bb
lucas-gtd <lucasgoutaudier@gmail.com>
2021-03-24 14:04:23 +0100

    modal + réccupération des infos article

--- js/articles.js
+++ js/articles.js
@@ -1,102 +1,128 @@
-class Article{
-    constructor(techno, title, logo, desc, code, keywords){
-        this.techno = techno
-        this.title = title
-        this.logo = logo
-        this.desc = desc
-        this.code = code
-        this.keywords = keywords
-    }
+class Article {
+  constructor(techno, title, logo, desc, code, keywords) {
+    this.techno = techno;
+    this.title = title;
+    this.logo = logo;
+    this.desc = desc;
+    this.code = code;
+    this.keywords = keywords;
+  }
 }
 
 const list = [
-    new Article('html', 'table', '../img/HTML_logo.png','blabla', `
+  new Article(
+    'html',
+    'table',
+    '../img/HTML_logo.png',
+    'blabla',
+    `
     <form action="" method="get" class="form-example">
         <div class="form-example">
             <label for="name">Enter your name: </label>
             <input type="text" name="name" id="name" required>
         </div>
     </form>
-    `, 'table html test'),
-    new Article('html', 'button', '../img/HTML_logo.png','blabla', `<button>Example</button>`, 'button html test'),
-    new Article('html', 'h1', '../img/HTML_logo.png','blabla', `<h1>Example</h1>`, 'h1 title html test'),
-]
+    `,
+    'table html test'
+  ),
+  new Article(
+    'html',
+    'button',
+    '../img/HTML_logo.png',
+    'blabla',
+    `<button>Example</button>`,
+    'button html test'
+  ),
+  new Article(
+    'html',
+    'h1',
+    '../img/HTML_logo.png',
+    'blabla',
+    `<h1>Example</h1>`,
+    'h1 title html test'
+  ),
+];
 
-const articleListDiv = document.querySelector('.articlesList')
+const articleListDiv = document.querySelector('.articlesList');
 
 const displayArticle = (techno, title, logo, desc, code, keywords) => {
+  const article = document.createElement('article');
+  article.classList.add('article');
+  articleListDiv.appendChild(article);
 
-    const article = document.createElement('article')
-    article.classList.add('article')
-    articleListDiv.appendChild(article)
+  const articleImgAndFav = document.createElement('div');
+  articleImgAndFav.classList.add('articleImgAndFav');
+  article.appendChild(articleImgAndFav);
 
-    const articleImgAndFav = document.createElement('div')
-    articleImgAndFav.classList.add('articleImgAndFav')
-    article.appendChild(articleImgAndFav)
+  const img = document.createElement('img');
+  img.src = logo;
+  img.alt = keywords;
+  articleImgAndFav.appendChild(img);
 
-    const img = document.createElement('img')
-    img.src = logo
-    img.alt = keywords
-    articleImgAndFav.appendChild(img)
+  const articleFav = document.createElement('div');
+  articleFav.classList.add('articleFav');
+  articleImgAndFav.appendChild(articleFav);
 
-    const articleFav = document.createElement('div')
-    articleFav.classList.add('articleFav')
-    articleImgAndFav.appendChild(articleFav)
+  const favBtn = document.createElement('button');
+  favBtn.classList.add('favBtn');
+  favBtn.innerHTML = '<i data-feather="star"></i>';
+  articleFav.appendChild(favBtn);
 
-    const favBtn = document.createElement('button')
-    favBtn.classList.add('favBtn')
-    favBtn.innerHTML = '<i data-feather="star"></i>'
-    articleFav.appendChild(favBtn)
+  const titleDesc = document.createElement('div');
+  titleDesc.classList.add('titleDesc');
+  article.appendChild(titleDesc);
 
-    const titleDesc = document.createElement('div')
-    titleDesc.classList.add('titleDesc')
-    article.appendChild(titleDesc)
+  const articleTitle = document.createElement('div');
+  articleTitle.classList.add('articleTitle');
+  titleDesc.appendChild(articleTitle);
 
-    const articleTitle = document.createElement('div')
-    articleTitle.classList.add('articleTitle')
-    titleDesc.appendChild(articleTitle)
+  const h2Article = document.createElement('h2');
+  h2Article.innerHTML = title;
+  articleTitle.appendChild(h2Article);
 
-    const h2Article = document.createElement('h2')
-    h2Article.innerHTML = title
-    articleTitle.appendChild(h2Article)
+  const articleDescription = document.createElement('div');
+  articleDescription.classList.add('articleDescription');
+  articleDescription.innerHTML = desc;
+  titleDesc.appendChild(articleDescription);
 
-    const articleDescription = document.createElement('div')
-    articleDescription.classList.add('articleDescription')
-    articleDescription.innerHTML = desc
-    titleDesc.appendChild(articleDescription)
+  const articleSnippet = document.createElement('div');
+  articleSnippet.classList.add('articleSnippet');
+  article.appendChild(articleSnippet);
 
-    const articleSnippet = document.createElement('div')
-    articleSnippet.classList.add('articleSnippet')
-    article.appendChild(articleSnippet)
+  const btnCopy = document.createElement('button');
+  btnCopy.classList.add('btnCopy');
+  btnCopy.innerHTML = 'COPIER';
+  articleSnippet.appendChild(btnCopy);
 
-    const btnCopy = document.createElement('button')
-    btnCopy.classList.add('btnCopy')
-    btnCopy.innerHTML = 'COPIER'
-    articleSnippet.appendChild(btnCopy)
+  const pre = document.createElement('pre');
+  articleSnippet.appendChild(pre);
 
-    const pre = document.createElement('pre')
-    articleSnippet.appendChild(pre)
+  const codeElement = document.createElement('code');
+  codeElement.classList.add('langage');
+  codeElement.classList.add(techno);
+  codeElement.innerText = code;
+  pre.appendChild(codeElement);
 
-    const codeElement = document.createElement('code')
-    codeElement.classList.add('langage')
-    codeElement.classList.add(techno)
-    codeElement.innerText = code
-    pre.appendChild(codeElement)
+  const hr = document.createElement('hr');
+  article.appendChild(hr);
 
-    const hr = document.createElement('hr')
-    article.appendChild(hr)
+  const open = document.createElement('div');
+  open.classList.add('open');
+  article.appendChild(open);
 
-    const open = document.createElement('div')
-    open.classList.add('open')
-    article.appendChild(open)
+  const modalBtn = document.createElement('button');
+  modalBtn.classList.add('modalBtn');
+  modalBtn.innerHTML = '<i data-feather="plus-circle"></i>';
+  open.appendChild(modalBtn);
+};
 
-    const modalBtn = document.createElement('button')
-    modalBtn.classList.add('modalBtn')
-    modalBtn.innerHTML = '<i data-feather="plus-circle"></i>'
-    open.appendChild(modalBtn)
-
-}
-
-list.forEach(element => {
-    displayArticle(element.techno, element.title, element.logo, element.desc, element.code, element.keywords)
-})
+list.forEach((element) => {
+  displayArticle(
+    element.techno,
+    element.title,
+    element.logo,
+    element.desc,
+    element.code,
+    element.keywords
+  );
+});
--- js/fav.js
+++ js/fav.js
@@ -1,25 +1,60 @@
-// Get the modal
-var modal = document.getElementById("articleModal");
+// RECUPPERER LES ELEMENTS
+const buttons = document.querySelectorAll('.modalBtn');
+const modals = document.querySelector('.modals');
+const close = document.querySelectorAll('.modalClose');
 
-// Get the button that opens the modal
-var btn = document.querySelector(".modalBtn");
+const displayModal = (article) => {
+  const modalContainer = document.createElement('div');
+  modalContainer.classList.add('modal-container');
+  modals.appendChild(modalContainer);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  const modalTitleDiv = document.createElement('div');
+  modalTitleDiv.classList.add('modal-title');
+  modalContainer.appendChild(modalTitleDiv);
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+  const modalTitle = document.createElement('h2');
+  modalTitle.innerText = article.title;
+  modalTitleDiv.appendChild(modalTitle);
+
+  const svgImg = document.createElement('img');
+  svgImg.classList.add('close');
+  svgImg.src = '../img/x.svg';
+  modalTitleDiv.appendChild(svgImg);
+
+  // const svg = document.createElement('svg');
+  // svg.classList.add('modalClose');
+  // svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  // svg.setAttribute('width', '24');
+  // svg.setAttribute('height', '24');
+  // svg.setAttribute('viewBox', '0 0 24 24');
+  // svg.setAttribute('fill', 'none');
+  // svg.setAttribute('stroke', 'currentColor');
+  // svg.setAttribute('stroke-width', '2');
+  // svg.setAttribute('stroke-linecap', 'round');
+  // svg.setAttribute('stroke-linejoin', 'round');
+  // modalTitleDiv.appendChild(svg);
+
+  // const line1 = document.createElement('line');
+  // line1.setAttribute('x1', '18');
+  // line1.setAttribute('y1', '6');
+  // line1.setAttribute('x2', '6');
+  // line1.setAttribute('x2', '18');
+  // svg.appendChild(line1);
+
+  // const line2 = document.createElement('line');
+  // line2.setAttribute('x1', '6');
+  // line2.setAttribute('y1', '6');
+  // line2.setAttribute('x2', '18');
+  // line2.setAttribute('x2', '18');
+  // svg.appendChild(line2);
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  displayModal(list[i]);
+  buttons[i].addEventListener('click', () => {
+    document.querySelectorAll('.modal-container')[i].classList.add('show');
+  });
+  close[i].addEventListener('click', () => {
+    document.querySelectorAll('.modal-container')[i].classList.remove('show');
+  });
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
